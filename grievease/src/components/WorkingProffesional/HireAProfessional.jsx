c29b7f7a22258e85963c72acc4fd9502f0be5d63
Aditya210703 <vermaaditya2356@gmail.com>
2024-04-06 17:07:23 +0530

    added-proffesional-profiles

--- grievease/src/components/WorkingProffesional/HireAProfessional.jsx
+++ grievease/src/components/WorkingProffesional/HireAProfessional.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import ProfessionalCardHire from './ProfessionalCardHire'
+import React, { useEffect, useState } from 'react';
+import ProfessionalCardHire from './ProfessionalCardHire';
+import { database } from '../../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 const HireAProfessional = () => {
-  return (
-    
-    <div className=' flex flex-wrap'>
-       <ProfessionalCardHire/><ProfessionalCardHire/>
-       <ProfessionalCardHire/><ProfessionalCardHire/><ProfessionalCardHire/>
-       </div>
-  )
-}
+  const [professionalData, setProfessionalData] = useState([]);
 
-export default HireAProfessional
+  const fetchProfessionalData = async () => {
+    const userCollections = collection(database, 'WorkingProfessional');
+    const data = await getDocs(userCollections);
+    const fil = data.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    setProfessionalData(fil);
+  };
+
+  useEffect(() => {
+    fetchProfessionalData();
+  }, []);
+
+  return (
+    <div className='flex flex-wrap'>
+      {professionalData.map((props) => (
+        <ProfessionalCardHire key={props.id} {...props} />
+      ))}
+    </div>
+  );
+};
+
+export default HireAProfessional;
--- grievease/src/components/WorkingProffesional/ProfessionalCardHire.jsx
+++ grievease/src/components/WorkingProffesional/ProfessionalCardHire.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 import { FcHome } from "react-icons/fc";
 const ProfessionalCardHire = (props) => {
   return (
-    <div className='flex shadow-lg m-3 rounded-lg bg-white/20'>
-    <img src='https://content.jdmagicbox.com/comp/durg/e5/9999px788.x788.170916104749.x5e5/catalogue/friends-electrical-bhilai-sector-6-bhilai-electricians-9h4mzcda41.jpg' className='w-44 h-44 m-3 rounded-xl object-cover' alt='profile pic' />
+    <div className='flex shadow-lg m-3 w-full rounded-lg bg-white/20'>
+    <img 
+  src={props.profileImage} 
+  className='w-44 h-44 m-3 rounded-xl object-cover' 
+  style={{ objectFit: 'cover', objectPosition: 'center' }}
+  alt='profile pic' 
+/>
+
     <section className="flex flex-col justify-between w-full">
         <div className="flex justify-between items-center mt-4 mx-3">
-            <h1 className='text-lg font-bold text-gray-800'>Name</h1>
-            <h1 className='text-lg font-semi-bold bg-white rounded-xl px-4 text-gray-800'>Services</h1>
+            <h1 className='text-lg font-bold text-gray-800'>{`${props.firstName +' '+ props.lastName}`}</h1>
+            <h1 className='text-lg w-36 font-semi-bold bg-white rounded-xl px-4 text-gray-800'>{props.services}</h1>
             <p className="text-lg font-medium text-orange-950 py-3 flex items-center">
     <FcHome className='w-8 h-8 mr-2' />
-    Locality
+    {props.address}
 </p>
 
         </div>
         
-        <p className="text-sm text-gray-700 mt-2">Bio: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid, iste quos similique consequuntur officia omnis facere! Architecto rerum molestias obcaecati iure, ad debitis! Dicta tempora, numquam dolores qui voluptates eius!</p>
+        <p className="text-sm text-gray-700 mt-2 ">{props.bio}</p>
         <button className="ml-auto w-32 bg-orange-400 text-white mr-4 mb-3 px-4 py-2 mt-3 rounded-lg shadow-md hover:bg-orange-500 transition duration-300 ease-in-out">Contact</button>
     </section>
 </div>
